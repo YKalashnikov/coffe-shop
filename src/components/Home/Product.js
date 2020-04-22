dad6f94a04c786d4b79574fc2fba1a54ac2b9f47
YKalashnikov <uk2709@gmail.com>
2020-04-22 00:51:49 -0700

    coffe

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -15,7 +15,7 @@ export default function Product({ product }) {
               data-item-name={product.title}
               data-item-price={product.price}
               data-item-image={product.image.fluid.src}
-              data-item-url=""
+              data-item-url="https://happy-noyce-a12990.netlify.app/"
             >
               add to cart
             </button>
